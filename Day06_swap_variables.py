1d4a9c7272fab5ce82a4ba30e60be454dffd13c8
Jasmitha <jasmithaganji@gmail.com>
2026-06-14 18:24:28 +0530

    Day06_swap_variables.py

---
+++ Day06_swap_variables.py
@@ -0,0 +1,21 @@
+a=10
+b=20
+
+print("Before swapping:") 
+print("a =", a) 
+print("b =", b) 
+
+a, b=b, a
+
+print("After swapping:") 
+print("a =", a) 
+print("b =", b) 
+
+#output:
+#Before swapping:
+#a = 10
+#b = 20
+
+#After swapping:
+#a = 20
+#b = 10
