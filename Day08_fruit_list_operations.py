a4a5d92cb3a9167c354f575c6291ab5ff7dfa781
Jasmitha <jasmithaganji@gmail.com>
2026-06-16 17:53:33 +0530

    Day08_fruit_list_operations.py

---
+++ Day08_fruit_list_operations.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+
+fruits.append("fig")
+
+fruits.pop(1)
+
+print(fruits)
+
+# Output:
+# ['apple', 'cherry', 'date', 'elderberry', 'fig']
