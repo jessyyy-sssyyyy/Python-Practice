8bdab780d593b098df80b91444cfc749b0a59de3
Jasmitha <jasmithaganji@gmail.com>
2026-06-20 17:49:53 +0530

    Day12_unique_elements.py

---
+++ Day12_unique_elements.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 2, 3, 4, 4, 5, 1]
+
+unique_numbers = list(set(numbers))
+
+print("Original List:", numbers)
+print("Unique List:", unique_numbers)
+
+# Output:
+# Original List: [1, 2, 2, 3, 4, 4, 5, 1]
+# Unique List: [1, 2, 3, 4, 5]
