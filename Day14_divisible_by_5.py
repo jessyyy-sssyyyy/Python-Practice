74abe28922bba2cbfae408e851f5e34e9f780825
Jasmitha <jasmithaganji@gmail.com>
2026-06-22 17:46:21 +0530

    Day14_divisible_by_5.py

---
+++ Day14_divisible_by_5.py
@@ -0,0 +1,15 @@
+numbers = [10, 15, 23, 25, 30, 42, 50]
+
+print("Numbers divisible by 5:")
+
+for num in numbers:
+    if num % 5 == 0:
+        print(num)
+
+# Output:
+# Numbers divisible by 5:
+# 10
+# 15
+# 25
+# 30
+# 50
