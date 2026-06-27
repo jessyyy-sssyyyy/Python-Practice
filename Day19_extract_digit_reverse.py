caa9cc6b996c01fff317523b5026443020d553d0
Jasmitha <jasmithaganji@gmail.com>
2026-06-27 17:46:38 +0530

    Day19_extract_digit_reverse.py

---
+++ Day19_extract_digit_reverse.py
@@ -0,0 +1,15 @@
+number = 7536
+
+print("Digits in reverse order:")
+
+while number > 0:
+    digit = number % 10
+    print(digit)
+    number = number // 10
+
+# Output:
+# Digits in reverse order:
+# 6
+# 3
+# 5
+# 7
