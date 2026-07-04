d2b16c955cb7939d3302427318c9907887965e7c
Jasmitha <jasmithaganji@gmail.com>
2026-07-04 17:56:05 +0530

    Day26_leap_year.py

---
+++ Day26_leap_year.py
@@ -0,0 +1,9 @@
+year = 2026
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
+
+# Output:
+# 2026 is a leap year
