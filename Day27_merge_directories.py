813a5a6c6d3bdb5a238303c71fd7339f64b2ad77
Jasmitha <jasmithaganji@gmail.com>
2026-07-06 17:48:00 +0530

    Day27_merge_directories.py

---
+++ Day27_merge_directories.py
@@ -0,0 +1,16 @@
+dict1 = {
+    "a": 10,
+    "b": 20
+}
+
+dict2 = {
+    "c": 30,
+    "d": 40
+}
+
+dict1.update(dict2)
+
+print("Merged Dictionary:", dict1)
+
+# Output:
+# Merged Dictionary: {'a': 10, 'b': 20, 'c': 30, 'd': 40}
