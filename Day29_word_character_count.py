82b4b7cda9a00d6c3715d379d88cc688727c642d
Jasmitha <jasmithaganji@gmail.com>
2026-07-09 18:09:08 +0530

    Day29_word_character_count.py

---
+++ Day29_word_character_count.py
@@ -0,0 +1,11 @@
+words = ["apple", "banana", "cherry", "grapes", "mango"]
+
+for word in words:
+    print(word, "-", len(word), "characters")
+
+# Output:
+# apple - 5 characters
+# banana - 6 characters
+# cherry - 6 characters
+# grapes - 6 characters
+# mango - 5 characters
